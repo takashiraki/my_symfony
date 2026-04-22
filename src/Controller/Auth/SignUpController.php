9a2613da6b2dbdf97694e305f512697a8e4fa588
takashiraki <stgtbike4649@gmail.com>
2026-04-22 05:06:44 +0000

    wip

--- src/Controller/Auth/SignUpController.php
+++ src/Controller/Auth/SignUpController.php
@@ -6,8 +6,6 @@ namespace App\Controller\Auth;
 
 use App\Entity\Account;
 use Doctrine\ORM\EntityManagerInterface;
-use OpenTelemetry\API\Globals;
-use OpenTelemetry\API\Trace\StatusCode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +24,15 @@ final class SignUpController extends AbstractController
         ]);
     }
 
+    /**
+     * Sign up
+     *
+     * @see https://symfony.com/doc/current/security/csrf.html
+     *
+     * @param Request $http_request
+     *
+     * @return Response
+     */
     #[Route('/sign-up', name: 'app_sign_up_post', methods: ['POST'])]
     #[IsCsrfTokenValid('sign_up', tokenKey: '_csrf_token')]
     public function signUp(
@@ -34,72 +41,37 @@ final class SignUpController extends AbstractController
         EntityManagerInterface $entityManager,
         ValidatorInterface $validator,
     ): Response {
-        $tracer = Globals::tracerProvider()->getTracer('app');
-
-        // 親Span: sign-up処理全体
-        $span = $tracer->spanBuilder('sign-up')->startSpan();
-        $scope = $span->activate();
-
-        try {
-            $email = $http_request->request->get('email');
-            $password = $http_request->request->get('password');
-            $confirm_password = $http_request->request->get('password_confirm');
-
-            if ($password !== $confirm_password) {
-                $span->setStatus(StatusCode::STATUS_ERROR, 'Passwords do not match');
-                return $this->render('signup/index.html.twig', [
-                    'controller_name' => 'SignUpController',
-                    'email' => $email,
-                    'errors' => ['Passwords do not match.'],
-                ]);
-            }
-
-            // 子Span: バリデーション
-            $validateSpan = $tracer->spanBuilder('sign-up.validate')->startSpan();
-            $validateScope = $validateSpan->activate();
-            try {
-                $account = new Account();
-                $account->setEmail($email);
-                $account->setPassword($passwordHasher->hashPassword($account, $password));
-                $violations = $validator->validate($account);
-            } finally {
-                $validateScope->detach();
-                $validateSpan->end();
-            }
-
-            if (count($violations) > 0) {
-                $errors = array_map(fn($v) => $v->getMessage(), iterator_to_array($violations));
-                $span->setStatus(StatusCode::STATUS_ERROR, 'Validation failed');
-                return $this->render('signup/index.html.twig', [
-                    'controller_name' => 'SignUpController',
-                    'email' => $email,
-                    'errors' => $errors,
-                ]);
-            }
-
-            // 子Span: DB保存
-            $persistSpan = $tracer->spanBuilder('sign-up.persist')->startSpan();
-            $persistScope = $persistSpan->activate();
-            try {
-                $entityManager->persist($account);
-                $entityManager->flush();
-            } finally {
-                $persistScope->detach();
-                $persistSpan->end();
-            }
-
-            // メトリクス: 登録成功数をカウント
-            Globals::meterProvider()
-                ->getMeter('app')
-                ->createCounter('user.signup.success')
-                ->add(1);
-
-            $span->setStatus(StatusCode::STATUS_OK);
-            return $this->redirectToRoute('app_login');
-
-        } finally {
-            $scope->detach();
-            $span->end();
+        $email = $http_request->request->get('email');
+        $password = $http_request->request->get('password');
+        $confirm_password = $http_request->request->get('password_confirm');
+        if ($password !== $confirm_password) {
+            return $this->render('signup/index.html.twig', [
+                'controller_name' => 'SignUpController',
+                'email' => $email,
+                'errors' => ['Passwords do not match.'],
+            ]);
         }
+
+        $account = new Account();
+        $account->setEmail($email);
+        $account->setPassword($passwordHasher->hashPassword($account, $password));
+        $violations = $validator->validate($account);
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getMessage();
+            }
+            return $this->render('signup/index.html.twig', [
+                'controller_name' => 'SignUpController',
+                'email' => $email,
+                'errors' => $errors,
+            ]);
+        }
+
+        $entityManager->persist($account);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_login');
     }
 }
