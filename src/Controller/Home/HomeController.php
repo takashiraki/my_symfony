b2874dd7b7a386b4bd14e8a22f4e8cc47f1ea02d
takashiraki <stgtbike4649@gmail.com>
2026-05-30 07:48:31 +0000

    wip

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Home;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        TaskRepository $tasksRepository,
+    ): Response {
+        /** @var \App\Entity\Account|null $account */
+        $account = $this->getUser();
+        $user = $account?->getUser();
+        $tasks = $user ? $tasksRepository->findByUser($user) : [];
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Home/HomeController',
+            'tasks' => $tasks,
         ]);
     }
 }
